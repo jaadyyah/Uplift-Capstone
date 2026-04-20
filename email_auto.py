cf68c9a2f7c8399d42b967ce92558b6c7b10d275
rsmaller <ryjobobo@gmail.com>
2026-04-20 14:32:36 -0400

    Made chnage to email erroring

--- email_auto.py
+++ email_auto.py
@@ -4,7 +4,7 @@ ses = boto3.client("ses", region_name="us-east-1")
 def send_email(user_email, body):
     return ses.send_email(
         Source="mail@upliftcompanyusa.com",
-        Destination={"ToAddresses": ["upliftcomapny123@gmail.com"]},
+        Destination={"ToAddresses": ["mail@upliftcompanyusa.com"]},
         Message={
             "Subject": {"Data": "Hello"},
             "Body": {"Text": {"Data": body}}
