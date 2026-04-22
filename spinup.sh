5a562d7c16b93ef1155db642950d9124458ba745
ryan <ryjobobo@gmail.com>
2026-04-22 15:35:13 -0400

    improved server startup again

--- spinup.sh
+++ spinup.sh
@@ -1,2 +1,7 @@
-# gunicorn -w 4 app:app -b 0.0.0.0:8000 # for initial use if necessary.
-sudo systemctl restart gunicorn
+kill -TERM $(cat /tmp/gunicorn.pid)
+
+while kill -0 $(cat /tmp/gunicorn.pid) 2>/dev/null; do
+    sleep 0.5
+done
+
+gunicorn -w 4 app:app -b 0.0.0.0:8000 --pid /tmp/gunicorn.pid --daemon
