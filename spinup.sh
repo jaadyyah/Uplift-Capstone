cc8b1444be3abd0b6e5bb23a9bbbc1c2e9103459
ryan <ryjobobo@gmail.com>
2026-04-22 15:40:52 -0400

    fixed race condition and edge case where server has not been started yet

--- spinup.sh
+++ spinup.sh
@@ -1,7 +1,10 @@
-kill -TERM $(cat /tmp/gunicorn.pid)
-
-while kill -0 $(cat /tmp/gunicorn.pid) 2>/dev/null; do
-    sleep 0.5
-done
+if [ -f "/tmp/gunicorn.pid" ]; then
+    PID=$(cat /tmp/gunicorn.pid)
+    kill -TERM $PID
+    while kill -0 $PID 2>/dev/null; do
+        sleep 0.5
+    done
+    sleep 1
+fi
 
 gunicorn -w 4 app:app -b 0.0.0.0:8000 --pid /tmp/gunicorn.pid --daemon
